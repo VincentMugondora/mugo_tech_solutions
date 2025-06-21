b8ec7c0b8a1dec8889441849458f28627641a686
vincentmugondora <vinmugondora@gmail.com>
2025-06-21 15:32:34 +0200

    Add TestimonialsSection and GetFreeTrial components to Services page for enhanced user engagement and content variety.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,6 +7,8 @@ import MarketingHero from '../components/services/MarketingHero';
 import WhyChooseUs from '../components/services/WhyChooseUs';
 import TopServicesGrid from '../components/services/TopServicesGrid';
 import Testimonials from '../components/services/Testimonials';
+import TestimonialsSection from '../components/services/TestimonialsSection';
+import GetFreeTrial from '../components/services/GetFreeTrial';
 import ContactTrialSection from '../components/about/ContactTrialSection';
 import Footer from '../components/Footer';
 
@@ -19,6 +21,8 @@ const Services = () => {
       <MarketingHero />
       <WhyChooseUs />
       <TopServicesGrid />
+      <TestimonialsSection />
+      <GetFreeTrial />
       <ServicesHero />
       <ServicesIntro />
       <WhyChooseUs />
